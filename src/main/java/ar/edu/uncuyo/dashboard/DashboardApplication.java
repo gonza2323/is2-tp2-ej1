864f4104bf6d51a741b6126715997cbaddd01cc8
Agustin Olivares <faolivares3112@gmail.com>
2025-10-02 11:36:54 -0300

    add funcionalidad mensajes

--- src/main/java/ar/edu/uncuyo/dashboard/DashboardApplication.java
+++ src/main/java/ar/edu/uncuyo/dashboard/DashboardApplication.java
@@ -2,7 +2,10 @@ package ar.edu.uncuyo.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+
+@EnableAsync
 @SpringBootApplication
 public class DashboardApplication {
 
